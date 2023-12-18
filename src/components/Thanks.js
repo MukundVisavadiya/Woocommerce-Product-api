13c62258e70a8c5f8f1b4228a0ec12b7b4e00c21
MukundVisavadiya <131982370+MukundVisavadiya@users.noreply.github.com>
2023-12-18 11:48:25 +0530

    complitie woocommarce project react

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -10,8 +10,6 @@ function Thanks() {
     const dispatch = useDispatch();
     const orderData = useSelector((state) => state.orderData);
     console.log(orderData);
-    // console.log(orderData.orderDataItem.totals.total_price);
-    console.log();
 
     useEffect(() => {
         dispatch(
@@ -19,10 +17,18 @@ function Thanks() {
         );
     }, []);
 
+    if (!orderData.orderDataItem || !orderData.orderDataItem.items) {
+        return <div>
+            <h2 className='text-center mt-5'>
+                Error: Invalid data structure
+            </h2>
+        </div>;
+    }
+
     return (
         <>
             <div className='container py-5'>
-                <div className='border border-dark'>
+                <div>
                     <h2 className='text-center'>Thank You. Your Order has Been Received.</h2>
                 </div>
                 <div className='py-4'>
@@ -43,8 +49,37 @@ function Thanks() {
                         </tbody>
                     </table>
                 </div>
+                <h2 className='text-center'>Order Details</h2>
+                <div className='py-4'>
+                    <table className="table table-hover border border-dark">
+                        <thead>
+                            <tr>
+                                <th>PRODUCT</th>
+                                <th>TOTAL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orderData && orderData.orderDataItem && orderData.orderDataItem.items.map((element, index) => (
+                                <tr key={index}>
+                                    <td>{element && element.name} × {element && element.quantity}</td>
+                                    <td>{element && (element.prices.price / 100).toFixed(2)}</td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <th>Total</th>
+                                <td>{(orderData.orderDataItem.totals.total_price / 100).toFixed(2)}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <h2 className='text-center'>Billing Address</h2>
                 <div className='border border-dark'>
-                    <h2 className='text-center'>Order Details</h2>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.first_name}{orderData.orderDataItem.billing_address.last_name}</p>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.address_1}</p>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.address_2}</p>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.city}{orderData.orderDataItem.billing_address.postcode}</p>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.state}</p>
+                    <p style={{ margin: '0', padding: '5px 10px' }}>{orderData.orderDataItem.billing_address.phone}</p>
                 </div>
             </div>
         </>
